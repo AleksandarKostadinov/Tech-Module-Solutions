a116276c6950576b41ba7da96188d2f7812ef67c
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-26 16:36:43 +0200

    Added 07. Sol

---
+++ Programming Fundamentals - януари 2017/L.03.MethodsAndDebugging/07.GreaterofTwoValues/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.GreaterofTwoValues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string variable = Console.ReadLine();
+
+            switch (variable)
+            {
+                case "int":
+                    int firstInt = int.Parse(Console.ReadLine());
+                    int secondInt = int.Parse(Console.ReadLine());
+                    int max = GetMax(firstInt, secondInt);
+                    Console.WriteLine(max);
+                    break;
+                case "char":
+                    char firstChar = char.Parse(Console.ReadLine());
+                    char secondChar = char.Parse(Console.ReadLine());
+                    char maxChar = (char)GetMax((int)firstChar, (int)secondChar);
+                    Console.WriteLine(maxChar);
+                    break;
+                case "string":
+                    string firstStr = Console.ReadLine();
+                    string secondStr = Console.ReadLine();
+                    string maxStr = GetMax(firstStr, secondStr);
+                    Console.WriteLine(maxStr);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public static string GetMax(string firstStr, string secondStr)
+        {
+            if (firstStr.CompareTo(secondStr) > 0)
+            {
+                return firstStr;
+            }
+            else
+            {
+                return secondStr;
+            }
+        }
+
+        public static int GetMax(int first, int second)
+        {
+            if (second > first)
+            {
+                return second;
+            }
+            else
+            {
+                return first;
+            }
+        }
+    }
+}
