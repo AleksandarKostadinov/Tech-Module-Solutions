bd1c3de46d96a50a484527712c25fd833bc77e9b
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-26 15:10:05 +0200

    Added Print Triangle Sol.

---
+++ Programming Fundamentals - януари 2017/L.03.MethodsAndDebugging/03.PrintingTriangle/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.PrintingTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parameter = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i < parameter; i++)
+            {
+                PrintLine(i);
+            }
+
+            PrintLine(parameter);
+
+            for (int i = parameter -1; i > 0; i--)
+            {
+                PrintLine(i);
+            }
+        }
+
+        public static void PrintLine(int lineLenght)
+        {
+            for (int i = 1; i <= lineLenght; i++)
+            {
+                Console.Write(i + " ");
+            }
+
+            Console.WriteLine();
+        }
+    }
+}
