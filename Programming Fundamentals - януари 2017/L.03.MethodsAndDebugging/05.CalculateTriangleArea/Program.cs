5ef171d8f214b684979cfa2ce13c2061abf29463
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-26 15:45:25 +0200

    Added 05. sol

---
+++ Programming Fundamentals - януари 2017/L.03.MethodsAndDebugging/05.CalculateTriangleArea/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.CalculateTriangleArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double width = double.Parse(Console.ReadLine());
+            double height = double.Parse(Console.ReadLine());
+            double area = GetTriangleAria(height , width);
+            Console.WriteLine(area);
+        }
+
+        static double GetTriangleAria(double height, double width)
+        {
+            return height * width / 2;
+        }
+    }
+}
