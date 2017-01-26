26969959ab819f0389f0fc6325fb5f9509997ca3
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-26 15:30:39 +0200

    Added 04. Draw a Filled Square Sol

---
+++ Programming Fundamentals - януари 2017/L.03.MethodsAndDebugging/04.DrawAFilledSquare/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.DrawAFilledSquare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int parameter = int.Parse(Console.ReadLine());
+
+            PrintFilledSquear(parameter);
+        }
+
+        public static void PrintFilledSquear(int parameter)
+        {
+            PrintTop(parameter);
+
+            for (int i = 0; i < parameter - 2; i++)
+            {
+                PrintMiddle(parameter);
+            }
+
+            PrintBottom(parameter);
+        }
+
+        public static void PrintTop(int parameter)
+        {
+            Console.WriteLine(new string('-', parameter * 2));
+        }
+
+        public static void PrintMiddle(int parameter)
+        {
+            Console.Write('-');
+            for (int i = 0; i < parameter - 1 ; i++)
+            {
+                Console.Write("\\/");
+            }
+
+            Console.WriteLine('-');
+        }
+
+        public static void PrintBottom(int parameter)
+        {
+            PrintTop(parameter);
+        }
+    }
+}
