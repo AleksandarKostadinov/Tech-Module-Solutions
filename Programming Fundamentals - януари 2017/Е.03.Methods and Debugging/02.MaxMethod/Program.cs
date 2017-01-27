091ca09bfb02706e93a6e0955db0f89a1e0e1841
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-27 14:40:50 +0200

    Added GetMax 02

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/02.MaxMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.MaxMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNum = int.Parse(Console.ReadLine());
+            int secondNum = int.Parse(Console.ReadLine());
+            int thirdNum = int.Parse(Console.ReadLine());
+
+            int max = GetMax(GetMax(firstNum, secondNum), thirdNum);
+            Console.WriteLine(max);
+        }
+
+        public static int GetMax(int firstNum, int secondNum)
+        {
+            return firstNum > secondNum ? firstNum : secondNum;
+        }
+    }
+}
