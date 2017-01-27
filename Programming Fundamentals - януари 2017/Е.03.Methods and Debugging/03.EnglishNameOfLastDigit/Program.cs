ab91db9dc5c3fdff351acfb45516df3b83e177cb
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-27 15:30:00 +0200

    Done to 04 EX.

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/03.EnglishNameOfLastDigit/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.EnglishNameOfLastDigit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var number = Console.ReadLine();
+
+            int digit = number[number.Length - 1] - '0';
+
+            string nameOfDigit = GetEngNameOfDigit(digit);
+
+            Console.WriteLine(nameOfDigit);
+        }
+
+        public static string GetEngNameOfDigit(int digit)
+        {
+            string nameOfDigit = string.Empty;
+            if (digit == 1)
+            {
+                nameOfDigit = "one";
+            }
+            else if (digit == 2)
+            {
+                nameOfDigit = "two";
+            }
+            else if (digit == 3)
+            {
+                nameOfDigit = "three";
+            }
+            else if (digit == 4)
+            {
+                nameOfDigit = "four";
+            }
+            else if (digit == 5)
+            {
+                nameOfDigit = "five";
+            }
+            else if (digit == 6)
+            {
+                nameOfDigit = "six";
+            }
+            else if (digit == 7)
+            {
+                nameOfDigit = "seven";
+            }
+            else if (digit == 8)
+            {
+                nameOfDigit = "eight";
+            }
+            else if (digit == 9)
+            {
+                nameOfDigit = "nine";
+            }
+            else if (digit == 0)
+            {
+                nameOfDigit = "zero";
+            }
+            return nameOfDigit;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/04.NumbersInReversedOrder/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.NumbersInReversedOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+
+            string revesed = GetReversedString(number);
+
+            Console.WriteLine(revesed);
+        }
+
+        public static string GetReversedString(string number)
+        {
+            string reversedStr = string.Empty;
+
+            for (int i = number.Length - 1; i >= 0; i--)
+            {
+                reversedStr += number[i];
+            }
+
+            return reversedStr;
+        }
+    }
+}
