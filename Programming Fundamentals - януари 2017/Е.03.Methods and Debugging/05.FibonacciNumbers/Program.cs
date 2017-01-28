2edcf06800b12fe4945a86fe7f1d9560cb03fea9
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-28 19:10:33 +0200

    Added Fibunacci Nums

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/05.FibonacciNumbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _05.FibonacciNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int result = GetFibunacci(n);
+
+            Console.WriteLine(result);
+        }
+
+        public static int GetFibunacci(int n)
+        {
+            int f0 = 1;
+            int f1 = 1;
+
+            for (int currentFibonacciNumber = 2; currentFibonacciNumber <= n
+                ; currentFibonacciNumber++)
+            {
+                int fibonacciNumber = f0 + f1;
+                f0 = f1;
+                f1 = fibonacciNumber;
+            }
+
+            return f1;
+        }
+    }
+}
