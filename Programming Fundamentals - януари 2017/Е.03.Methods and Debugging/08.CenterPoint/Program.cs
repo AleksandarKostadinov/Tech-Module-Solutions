009e19195d450b29c968f213704e4a21b5c7bc33
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-29 10:43:18 +0200

    Added 09.LongerLine

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/08.CenterPoint/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.CenterPoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+
+            double firstDistance = GetDistanceToCanter(x1, y1);
+            double secondDistance = GetDistanceToCanter(x2, y2);
+
+            if (firstDistance <= secondDistance)
+            {
+                PrintCoordinates(x1, y1);
+            }
+            else
+            {
+                PrintCoordinates(x2, y2);
+            }
+        }
+
+        public static void PrintCoordinates(double x, double y)
+        {
+            Console.Write($"({x}, {y})");
+        }
+
+        public static double GetDistanceToCanter(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/09.LongerLine/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.LongerLine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1 = double.Parse(Console.ReadLine());
+            double y1 = double.Parse(Console.ReadLine());
+            double x2 = double.Parse(Console.ReadLine());
+            double y2 = double.Parse(Console.ReadLine());
+            double x3 = double.Parse(Console.ReadLine());
+            double y3 = double.Parse(Console.ReadLine());
+            double x4 = double.Parse(Console.ReadLine());
+            double y4 = double.Parse(Console.ReadLine());
+
+            double firstLineLength = GetDistanceToCanter(x2 - x1, y2 - y1);
+            double secondLineLength = GetDistanceToCanter(x4 - x3, y4 - y3);
+
+            if (firstLineLength >= secondLineLength)
+            {
+                PrintClosest(x1, y1, x2, y2);
+            }
+            else
+            {
+                PrintClosest(x3, y3, x4, y4);
+            }
+        }
+
+        private static void PrintClosest(double x1, double y1, double x2, double y2)
+        {
+            if (GetDistanceToCanter(x1, y1) <= GetDistanceToCanter(x2, y2))
+            {
+                PrintCoordinates(x1, y1);
+                PrintCoordinates(x2, y2);
+            }
+            else
+            {
+                PrintCoordinates(x2, y2);
+                PrintCoordinates(x1, y1);
+            }
+
+            Console.WriteLine();
+        }
+
+        public static double GetDistanceToCanter(double x, double y)
+        {
+            return Math.Sqrt(x * x + y * y);
+        }
+
+        public static void PrintCoordinates(double x, double y)
+        {
+            Console.Write($"({x}, {y})");
+        }
+    }
+}
