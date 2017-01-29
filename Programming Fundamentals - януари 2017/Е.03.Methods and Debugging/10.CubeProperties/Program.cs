2b5815632cf5ad1b990ea7f06bb898dfd9e1e940
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-29 11:12:34 +0200

    Added Geometry Properties

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/10.CubeProperties/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _10.CubeProperties
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double side = double.Parse(Console.ReadLine());
+            string property = Console.ReadLine();
+
+            if (property == "face")
+            {
+                double faceDiagonal = Math.Round(GetFaceDiagonal(side), 2);
+                Console.WriteLine("{0:f2}",faceDiagonal);
+            }
+            else if (property == "space")
+            {
+                double spaceDiagonal = Math.Round(GetSpaceDiagonal(side) , 2);
+                Console.WriteLine("{0:f2}", spaceDiagonal);
+            }
+            else if (property == "volume")
+            {
+                double volume = Math.Round(GetCubeVolume(side), 2);
+
+                Console.WriteLine("{0:f2}", volume);
+            }
+            else if (property == "area")
+            {
+                double area = Math.Round(GetCubeArea(side) , 2);
+
+                Console.WriteLine("{0:f2}",area);
+            }
+        }
+
+        private static double GetCubeArea(double side)
+        {
+            return side * side * 6;
+        }
+
+        private static double GetCubeVolume(double side)
+        {
+            return side * side * side;
+        }
+
+        private static double GetSpaceDiagonal(double side)
+        {
+            return Math.Sqrt(side * side + side * side + side * side);
+        }
+
+        private static double GetFaceDiagonal(double side)
+        {
+            return Math.Sqrt(side * side + side * side);
+        }
+    }
+}
