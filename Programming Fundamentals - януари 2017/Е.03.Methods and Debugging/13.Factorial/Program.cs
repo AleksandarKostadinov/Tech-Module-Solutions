624b26b55c4d2b84508cd2aa1c2dc4a36a613ad3
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-29 14:57:26 +0200

    Added Factoriel

---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/13.Factorial/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _13.Factorial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            BigInteger factorielNum = FactorialNumber(n);
+
+            Console.WriteLine(factorielNum);
+        }
+
+        public static BigInteger FactorialNumber(int n)
+        {
+            BigInteger factoriel = 1;
+            while (n > 1)
+            {
+                factoriel *= n;
+                n--;
+            }
+
+            return factoriel;
+        }
+    }
+}
