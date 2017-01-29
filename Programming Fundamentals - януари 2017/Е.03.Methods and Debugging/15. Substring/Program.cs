915a541edef1cd07ea38413515b4c4192e5cd18d
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-29 19:23:44 +0200

    Added 15.Substring , 16.InstructionsSet and 17.BePositive

--- Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/15. Substring/Program.cs
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/15. Substring/Program.cs
@@ -1,15 +1,37 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace _15.Substring
+public class Substring_broken
 {
-    class Program
+    public static void Main()
     {
-        static void Main(string[] args)
+        string text = Console.ReadLine();
+        int jump = int.Parse(Console.ReadLine());
+
+        const char Search = 'p';
+        bool hasMatch = false;
+
+        for (int i = 0; i < text.Length; i++)
         {
+            if (text[i] == Search)
+            {
+                hasMatch = true;
+
+                int lenghtOfSub = jump + 1;
+
+                if (lenghtOfSub + i > text.Length )
+                {
+                    lenghtOfSub =text.Length - i ;
+                }
+
+                string matchedString = text.Substring(i, lenghtOfSub);
+                Console.WriteLine(matchedString);
+                i += jump;
+            }
+        }
+
+        if (!hasMatch)
+        {
+            Console.WriteLine("no");
         }
     }
 }
---
+++ Programming Fundamentals - януари 2017/Е.03.Methods and Debugging/16. InstructionSet/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class InstructionSet_broken
+{
+    static void Main()
+    {
+        do
+        {
+            string input = Console.ReadLine();
+
+            string[] codeArgs = input.Split(' ');
+
+            long result = 0;
+
+            if (codeArgs[0] == "END")
+            {
+                break;
+            }
+            else if (codeArgs[0] == "INC")
+            {
+                long operandOne = long.Parse(codeArgs[1]);
+                result = operandOne + 1;
+            }
+            else if (codeArgs[0] == "DEC")
+            {
+                long operandOne = long.Parse(codeArgs[1]);
+                result = --operandOne;
+            }
+            else if (codeArgs[0] == "ADD")
+            {
+                long operandOne = long.Parse(codeArgs[1]);
+                long operandTwo = long.Parse(codeArgs[2]);
+                result = operandOne + operandTwo;
+            }
+            else if (codeArgs[0] == "MLA")
+            {
+                long operandOne = long.Parse(codeArgs[1]);
+                long operandTwo = long.Parse(codeArgs[2]);
+                result = operandOne * operandTwo;
+            }
+            
+            Console.WriteLine(result);
+        } while (true);
+    }
+}
