86927648040feaa8c2d977ac27febee73b7d6e3a
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-30 13:56:30 +0200

    Added 01. Day of Week and 02. ReversedArray

--- Programming Fundamentals - януари 2017/L.04.Arrays/01.DayOfWeek/Program.cs
+++ Programming Fundamentals - януари 2017/L.04.Arrays/01.DayOfWeek/Program.cs
@@ -10,6 +10,22 @@ namespace _01.DayOfWeek
     {
         static void Main(string[] args)
         {
+            int numOfDay = int.Parse(Console.ReadLine());
+
+            PrintDayOfWeek(numOfDay);
+        }
+
+        public static void PrintDayOfWeek(int numOfDay)
+        {
+            var daysOfWeek = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (numOfDay >= 1 && numOfDay <=7 )
+            {
+                Console.WriteLine(daysOfWeek[numOfDay - 1]);
+            }
+            else
+            {
+                Console.WriteLine("Invalid Day!");
+            }
         }
     }
 }
---
+++ Programming Fundamentals - януари 2017/L.04.Arrays/02.ReverseArrayOfIntegers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02.ReverseArrayOfIntegers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lengthOfArr = int.Parse(Console.ReadLine());
+            int[] arr= GetArrayOfInts(lengthOfArr);
+
+            Console.WriteLine(string.Join(" ", arr.Reverse()));
+        }
+
+        public static int[] GetArrayOfInts(int lengthOfArr)
+        {
+            int[] arr = new int[lengthOfArr];
+            for (int currentElement = 0; currentElement < lengthOfArr; currentElement++)
+            {
+                arr[currentElement] = int.Parse(Console.ReadLine());
+            }
+
+            return arr;
+        }
+    }
+}
