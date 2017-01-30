3d8b736c6c69878cd08a8c8b636e0107e6fe8b77
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-30 20:04:09 +0200

    Added Extract Middle Element . Solution Done.

---
+++ Programming Fundamentals - януари 2017/L.04.Arrays/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var nums = ReadNumbersFromLine();
+            var condensed = new int[nums.Length - 1];
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 0; j < nums.Length - 1; j++)
+                {
+                    condensed[j] = nums[j] + nums[j + 1];
+                }
+                if (condensed.Length > 0)
+                {
+                    nums = condensed;
+                }
+            }
+            Console.WriteLine(nums[0]);
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/L.04.Arrays/09.ExtractMiddleElements/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _09.ExtractMiddleElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ReadNumbersFromLine();
+
+            if (numbers.Length == 1)
+            {
+                Console.WriteLine("{ " + numbers[0] + " }");
+            }
+            else if (numbers.Length % 2 == 0)
+            {
+                Console.WriteLine("{ " + numbers[numbers.Length / 2 - 1] + ", " + numbers[numbers.Length / 2] + " }");
+            }
+            else
+            {
+                Console.WriteLine("{ " + numbers[numbers.Length / 2 - 1] + ", " + numbers[numbers.Length / 2]
+                    + ", " + numbers[numbers.Length / 2 + 1] + " }");
+            }
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
