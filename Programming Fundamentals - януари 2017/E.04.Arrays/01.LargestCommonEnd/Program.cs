c19f39df8412f21c22db0f63e4ecd1e71372badb
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-30 21:50:07 +0200

    Added Rotate And Sum

--- Programming Fundamentals - януари 2017/E.04.Arrays/01.LargestCommonEnd/Program.cs
+++ Programming Fundamentals - януари 2017/E.04.Arrays/01.LargestCommonEnd/Program.cs
@@ -10,7 +10,26 @@ namespace _01.LargestCommonEnd
     {
         static void Main(string[] args)
         {
+            string[] firstWordArr = Console.ReadLine().Split(' ');
+            string[] secondWordArr = Console.ReadLine().Split(' ');
 
+            int leftEqualsCount = 0;
+            int rightEqualsCount = 0;
+
+            int lengthOfShorterArr = Math.Min(firstWordArr.Length, secondWordArr.Length);
+
+            for (int i = 0; i < lengthOfShorterArr; i++)
+            {
+                if (firstWordArr[i] == secondWordArr[i])
+                {
+                    leftEqualsCount++;
+                }
+                if (firstWordArr[firstWordArr.Length - 1 -i] == secondWordArr[secondWordArr.Length - 1 - i])
+                {
+                    rightEqualsCount++;
+                }
+            }
+            Console.WriteLine(Math.Max(leftEqualsCount, rightEqualsCount));
         }
     }
 }
---
+++ Programming Fundamentals - януари 2017/E.04.Arrays/02RotateAndSum/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02RotateAndSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = ReadNumbersFromLine();
+            int rotationsCount = int.Parse(Console.ReadLine());
+
+            int[] result = new int[arr.Length];
+
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int r = 1; r <= rotationsCount; r++)
+                {
+                    int theBigFormulaForCurrentN = ((arr.Length - r % arr.Length) + i) % arr.Length;
+                    int currentNumOfArr = arr[theBigFormulaForCurrentN];
+                    result[i] += currentNumOfArr;
+                }
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
