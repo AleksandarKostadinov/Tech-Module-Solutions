82231e60472d2047dfa18f856d54f206be8b3209
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-30 16:46:51 +0200

    Added Last K Numbers And Triple Sum

---
+++ Programming Fundamentals - януари 2017/L.04.Arrays/03.LastKNumbersSums/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.LastKNumbersSums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long n = long.Parse(Console.ReadLine());
+            long k = long.Parse(Console.ReadLine());
+
+            long[] sequence = new long[n];
+            if (n != 0)
+            {
+                sequence[0] = 1;
+                Console.Write(sequence[0] + " ");
+
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                long startOfSuming = 0;
+                if (i > k && !(k > n))
+                {
+                    startOfSuming = i - k ;
+                }
+
+                sequence[i] = SumOfArrInRange(startOfSuming, i, sequence);
+
+                Console.Write(sequence[i] + " ");
+            }
+        }
+
+        public static long SumOfArrInRange(long start, long end, long[] arr)
+        {
+            long sum = 0;
+            for (long i = start; i < end; i++)
+            {
+                sum += arr[i];
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/L.04.Arrays/04.TripplesSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.TripplesSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = ReadNumbersFromLine();
+            bool findTriple = false;
+            bool doNotOutputNo = true; ;
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    for (int k = 0; k < numbers.Length; k++)
+                    {
+                        findTriple = numbers[k] == numbers[i] + numbers[j];
+
+                        if (findTriple)
+                        {
+                            Console.WriteLine($"{numbers[i]} + {numbers[j]} == {numbers[k]}");
+                            doNotOutputNo = false;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (doNotOutputNo)
+            {
+                Console.WriteLine("No");
+            }
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
