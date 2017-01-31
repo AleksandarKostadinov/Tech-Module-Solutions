dc2717a524b08650cb707cdc2a168fffbd1c16c0
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-01-31 14:42:00 +0200

    Added 06. Max Sequence of Equal Elements, 07. Max Sequence of Increasing Elements and 08. Most Frequent Number

---
+++ Programming Fundamentals - януари 2017/E.04.Arrays/07.MaxSequenceOfIncreasingNums/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07.MaxSequenceOfIncreasingNums
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = ReadNumbersFromLine();
+
+            int bestStart = 0;
+            int start = 0;
+            int length = 1;
+            int bestLength = 1;
+
+            for (int i = 1; i < sequence.Length; ++i)
+            {
+                if (sequence[i] -  sequence[i - 1] >= 1)
+                {
+                    length++;
+                    if (length > bestLength)
+                    {
+                        bestStart = start;
+                        bestLength = length;
+                    }
+                }
+                else
+                {
+                    start = i;
+                    length = 1;
+                }
+            }
+
+            PrintArrFromWithLength(bestStart, bestLength, sequence);
+        }
+
+        public static void PrintArrFromWithLength(int start, int length, int[] sequence)
+        {
+            for (int i = start; i < start + length; i++)
+            {
+                Console.Write(sequence[i] + " ");
+            }
+
+            Console.WriteLine();
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/E.04.Arrays/08.MostFrequentNumber/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.MostFrequentNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = ReadNumbersFromLine();
+
+            int count = 1;
+            int mostFrequentNum = sequence[0];
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                int currentCount = GetFrequencyOfNumInArr(sequence, sequence[i]);
+
+                if (currentCount > count)
+                {
+                    count = currentCount;
+                    mostFrequentNum = sequence[i];
+                }
+            }
+
+            Console.WriteLine(mostFrequentNum);
+        }
+
+        public static int GetFrequencyOfNumInArr(int[] sequence, int number)
+        {
+            int count = 0;
+
+            for (int i = 0; i < sequence.Length; i++)
+            {
+                if (sequence[i] == number)
+                {
+                    count++;
+                }
+            }
+
+            return count;
+        }
+
+        public static int[] ReadNumbersFromLine()
+        {
+            int[] nums = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse).ToArray();
+
+            return nums;
+        }
+    }
+}
