e34fdc362b3ad38386f97e118ce91301725857f6
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-01 12:09:42 +0200

    Adeed in 05. Lists : 03. Sum of Adjacent Numbers and 04. Split by Word Casing

---
+++ Programming Fundamentals - януари 2017/L.05.Lists/03.SumAdjacentEqualNumbers/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03.SumAdjacentEqualNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<decimal> realNumbers = ReadListFrom();
+            
+            GetSumedAdjecantEqualNums(realNumbers);
+
+            Console.WriteLine(string.Join(" ", realNumbers));
+        }
+
+        public static void GetSumedAdjecantEqualNums(List<decimal> realNumbers)
+        {
+            for (int i = 0; i < realNumbers.Count -1 ; i++)
+            {
+                if (realNumbers[i] == realNumbers[i + 1])
+                {
+                    realNumbers[i] += realNumbers[i + 1];
+                    realNumbers.RemoveAt(i + 1);
+                    if (i != 0)
+                    {
+                        i -= 2;
+                    }
+                    else 
+                    {
+                        i--;
+                    }
+                }
+            }
+        }
+
+        public static List<decimal> ReadListFrom()
+        {
+            List<decimal> numbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            return numbers;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/L.05.Lists/04.SplitByWordCasing/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04.SplitByWordCasing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = ReadListFrom();
+            var lowerCaseWords = new List<string>();
+            var upperCaseWords = new List<string>();
+            var mixedCaseWords = new List<string>();
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                bool isLowerCaseWord = GetIsLowerCaseword(words[i]);
+                if (isLowerCaseWord)
+                {
+                    lowerCaseWords.Add(words[i]);
+                }
+                else if (GetIsUpperCaseWord(words[i]))
+                {
+                    upperCaseWords.Add(words[i]);
+                }
+                else
+                {
+                    mixedCaseWords.Add(words[i]);
+                }
+            }
+
+            Console.WriteLine("Lower-case: " + string.Join(", ", lowerCaseWords));
+            Console.WriteLine("Mixed-case: " + string.Join(", ", mixedCaseWords));
+            Console.WriteLine("Upper-case: " + string.Join(", ", upperCaseWords));
+        }
+
+        public static bool GetIsUpperCaseWord(string word)
+        {
+            bool upperCaseOnly = true; 
+
+            foreach (var symbol in word)
+            {
+                if (symbol < 65 || symbol > 90)
+                {
+                    upperCaseOnly = false;
+                }
+            }
+
+            return upperCaseOnly;
+        }
+
+        public static bool GetIsLowerCaseword(string word)
+        {
+            bool lowerCaseOnly = true;
+
+            foreach (var symbol in word)
+            {
+                if (symbol < 97 || symbol > 122)
+                {
+                    lowerCaseOnly = false;
+                }
+            }
+
+            return lowerCaseOnly;
+        }
+
+        public static List<string> ReadListFrom()
+        {
+            List<string> words = Console.ReadLine()
+                .Split(new char[] { ' ', ',', ';', ':', '.', '!', '(', ')', '"',  '\'', '\\', '/', '[', ']' }
+                , StringSplitOptions.RemoveEmptyEntries).ToList();
+
+            return words;
+        }
+    }
+}
