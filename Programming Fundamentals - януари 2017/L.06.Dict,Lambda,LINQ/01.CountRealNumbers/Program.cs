906bc7e7d966a4b38f9370b2b2727696c76ac7d4
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-03 15:15:55 +0200

    Added New Sol L.06. Dictionaries, Lambda And LINQ. In Sol: 01.Count Real Numbers and 02.Odd Occurrence

---
+++ Programming Fundamentals - януари 2017/L.06.Dict,Lambda,LINQ/01.CountRealNumbers/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace _01.CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Program
+    {
+        public static void Main()
+        {
+            var numbers = ReadListOfNumbers();
+
+            var countOfNums = new SortedDictionary<decimal, int>();
+
+            foreach (var item in numbers)
+            {
+                if (!countOfNums.ContainsKey(item))
+                {
+                    countOfNums[item] = 0;
+                }
+                countOfNums[item]++;
+            }
+
+            foreach (var number in countOfNums)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }  
+        }
+
+        public static List<decimal> ReadListOfNumbers()
+        {
+            List<decimal> numbers = Console.ReadLine().Split(new char[] { ' ', ',' }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(decimal.Parse)
+                .ToList();
+
+            return numbers;
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/L.06.Dict,Lambda,LINQ/02.OddOccurrences/OddcountOfWords.cs
@@ -0,0 +1,41 @@
+﻿namespace _02.OddOccurrences
+{
+    using System;
+    using System.Collections.Generic; 
+    using System.Linq;
+
+    public class OddCountOfWords
+    {
+        public static void Main()
+        {
+            var strs = Console.ReadLine()
+                .ToLower()
+                .Split(' ')
+                .ToList();
+
+            var oddCount = new Dictionary<string, int>();
+
+            foreach (var s in strs)
+            {
+                if (!oddCount.ContainsKey(s))
+                {
+                    oddCount[s] = 0;
+                }
+
+                oddCount[s]++;
+            }
+
+            var result = new List<string>();
+
+            foreach (var item in oddCount)
+            {
+                if (item.Value % 2 == 1)
+                {
+                    result.Add(item.Key);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", result));
+        }
+    }
+}
