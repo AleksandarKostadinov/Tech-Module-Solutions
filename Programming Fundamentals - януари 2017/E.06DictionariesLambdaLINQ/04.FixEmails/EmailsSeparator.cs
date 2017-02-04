72d8d3dc76bf28b8d0a419d64e99bca9a55a4e16
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-04 13:57:57 +0200

    Added 04.FixEmails and in E.06

---
+++ Programming Fundamentals - януари 2017/E.06DictionariesLambdaLINQ/04.FixEmails/EmailsSeparator.cs
@@ -0,0 +1,35 @@
+﻿namespace _04.FixEmails
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class EmailsSeparator
+    {
+        static void Main()
+        {
+            var line = Console.ReadLine();
+            var emailContacts = new Dictionary<string, string>();
+
+            while (line != "stop")
+            {
+                var name = line;
+                var email = Console.ReadLine();
+
+                if (!email.ToLower().EndsWith("us") && !email.ToLower().EndsWith("uk"))
+                {
+                    emailContacts[name] = email;
+                }
+
+                line = Console.ReadLine();
+            }
+
+            foreach (var contact in emailContacts)
+            {
+                var name = contact.Key;
+                var email = contact.Value;
+                Console.WriteLine($"{name} -> {email}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/E.06DictionariesLambdaLINQ/05.HandsOfCards/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace _05.HandsOfCards
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
