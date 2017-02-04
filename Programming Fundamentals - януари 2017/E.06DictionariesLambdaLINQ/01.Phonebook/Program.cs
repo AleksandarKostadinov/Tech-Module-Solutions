23fcba7e7e922528b951479a5b509424a55cbcda
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-04 12:39:32 +0200

    Added New Sol E.06. Dictionaries, Lambda And LINQ with new program 01.Phonebook

---
+++ Programming Fundamentals - януари 2017/E.06DictionariesLambdaLINQ/01.Phonebook/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01.Phonebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var tokens = Console.ReadLine()
+                .Split();
+            var contacts = new Dictionary<string, string>();
+
+            while (tokens[0] != "END")
+            {
+                var command = tokens[0];
+                var name = tokens[1];
+
+                if (command == "A")
+                {
+                    var phoneNumber = tokens[2];
+                    contacts[name] = phoneNumber;
+                }
+                else if (command == "S")
+                {
+                    if (contacts.ContainsKey(name))
+                    {
+                        var phoneNumber = contacts[name];
+                        Console.WriteLine($"{name} -> {phoneNumber}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+
+                tokens = Console.ReadLine()
+                    .Split();
+            }
+        }
+    }
+}
