7485d9ae98a871aea443aaf182bb6d6800be1e76
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-04 13:25:30 +0200

    Added 02.Phonebook Upgade and 03. A Miner Task in sol E.06

---
+++ Programming Fundamentals - януари 2017/E.06DictionariesLambdaLINQ/02.PhonebookUpgrade/ContactsUpgrade.cs
@@ -0,0 +1,52 @@
+﻿namespace _02.PhonebookUpgrade
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class ContactsUpgrade
+    {
+        static void Main()
+        {
+            var tokens = Console.ReadLine()
+               .Split();
+            var contacts = new SortedDictionary<string, string>();
+
+            while (tokens[0] != "END")
+            {
+                var command = tokens[0];
+
+                if (command == "A")
+                {
+                    var name = tokens[1];
+                    var phoneNumber = tokens[2];
+                    contacts[name] = phoneNumber;
+                }
+                else if (command == "S")
+                {
+                    var name = tokens[1];
+
+                    if (contacts.ContainsKey(name))
+                    {
+                        var phoneNumber = contacts[name];
+                        Console.WriteLine($"{name} -> {phoneNumber}");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Contact {name} does not exist.");
+                    }
+                }
+                else if (command == "ListAll")
+                {
+                    foreach (var contact in contacts)
+                    {
+                        Console.WriteLine($"{contact.Key} -> {contact.Value}");
+                    }
+                }
+
+                tokens = Console.ReadLine()
+                    .Split();
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals - януари 2017/E.06DictionariesLambdaLINQ/03AMinerTask/CollectResources.cs
@@ -0,0 +1,38 @@
+﻿namespace _03AMinerTask
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CollectResources
+    {
+        static void Main()
+        {
+            var resource = Console.ReadLine();
+            var resourceQuantity = new Dictionary<string, long>();
+
+            while (resource != "stop")
+            {
+                var quantity = long.Parse(Console.ReadLine());
+
+                if (!resourceQuantity.ContainsKey(resource))
+                {
+                    resourceQuantity[resource] = 0;
+                }
+
+                resourceQuantity[resource] += quantity;
+
+                resource = Console.ReadLine();
+            }
+
+            foreach (var pair in resourceQuantity)
+            {
+                if (resourceQuantity.Count > 0)
+                {
+
+                }
+                Console.WriteLine($"{pair.Key} -> {pair.Value}");
+            }
+        }
+    }
+}
