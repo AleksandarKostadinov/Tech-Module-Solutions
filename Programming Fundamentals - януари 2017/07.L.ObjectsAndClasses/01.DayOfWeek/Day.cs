040e3fa8f2b83fa4ba04bb2f0f1ddea772d3256b
AleksandarKostadinov <aleksandarkostadinov555@gmail.com>
2017-02-07 19:55:02 +0200

    Added New Solution 07.L. Objects and Classes, and 01.DayOfWeek whitin

---
+++ Programming Fundamentals - януари 2017/07.L.ObjectsAndClasses/01.DayOfWeek/Day.cs
@@ -0,0 +1,18 @@
+﻿namespace _01.DayOfWeek
+{
+    using System;
+    using System.Globalization;
+    public class Day
+    {
+        public static void Main()
+        {
+            var dateStr = Console.ReadLine();
+            var date = DateTime.ParseExact(
+                dateStr, "d-M-yyyy"
+                , CultureInfo.InvariantCulture);
+
+            Console.WriteLine(date.DayOfWeek);
+
+        }
+    }
+}
